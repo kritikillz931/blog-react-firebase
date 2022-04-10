d94ee4cd37abba2141967ede1b4e24faef9f9db6
Justin <justinadams758@gmail.com>
2022-04-09 18:01:17 -0600

    authentication setup correctly

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { auth, provider } from "../firebase-config"
+import { signInWithPopup } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
-export const Login = () => {
+
+export const Login = ({ setIsAuth }) => {
+
+  let navigate = useNavigate()
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true)
+      setIsAuth(true)
+      navigate("/")
+    })
+  }
   return (
-    <div>Login</div>
+    <div className='loginPage'>
+      <p>Sign In With Google To Continue</p>
+      <button className='loginWithGoogleButton' onClick={signInWithGoogle}>
+        Sign In With Google
+      </button>
+    </div>
   )
 }
